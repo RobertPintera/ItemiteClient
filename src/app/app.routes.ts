05d8093373eb729dbfa098f3fc0e9210f250cae3
RobertPintera <robert.it.990@gmail.com>
2025-09-29 23:33:30 +0200

    Prepare project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './components/layouts/main-layout/main-layout';
+import { Home } from './components/features/home/home';
+import { Login } from './components/features/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        component: Home,
+      },
+      {
+        path: 'login',
+        component: Login,
+      }
+    ]
+  }
+];
