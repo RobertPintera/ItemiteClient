92929f025c6a50888194f1ec5cc27869134aca7f
RobertPintera <robert.it.990@gmail.com>
2025-09-29 19:44:43 +0200

    Init new project - remove scss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,3 @@
 import { Routes } from '@angular/router';
-import { MainLayout } from './components/layouts/main-layout/main-layout';
-import { Home } from './components/features/home/home';
-import { Login } from './components/features/login/login';
 
-export const routes: Routes = [
-  {
-    path: '',
-    component: MainLayout,
-    children: [
-      {
-        path: '',
-        component: Home,
-      },
-      {
-        path: 'login',
-        component: Login,
-      }
-    ]
-  }
-];
+export const routes: Routes = [];
