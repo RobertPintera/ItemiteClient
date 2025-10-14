a142f9a37ddef093b2e36074a271c5de7060796b
RobertPintera <robert.it.990@gmail.com>
2025-10-15 00:12:47 +0200

    Add product details page and adjust style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { MainLayout } from './components/layouts/main-layout/main-layout';
 import { Home } from './components/features/home/home';
 import { Login } from './components/features/login/login';
 import {ProductsList} from './components/features/products-list/products-list';
+import {ProductDetails} from './components/features/product-details/product-details';
 
 export const routes: Routes = [
   {
@@ -16,6 +17,10 @@ export const routes: Routes = [
       {
         path: 'products/:category',
         component: ProductsList
+      },
+      {
+        path: 'products/:category/:slug',
+        component: ProductDetails
       },
       {
         path: 'login',
--- src/app/components/features/products-list/product-list-view/product-item/product-item.ts
+++ src/app/components/features/products-list/product-list-view/product-item/product-item.ts
@@ -1,11 +1,13 @@
 import {Component, computed, input} from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
 import {Product} from '../../../../../core/models/Product';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-product-item',
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    RouterLink
   ],
   templateUrl: './product-item.html',
   styleUrl: './product-item.css'
