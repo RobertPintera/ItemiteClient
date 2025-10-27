8931986c5239ef6bdd91e0100bbc7174a30e7395
HAScrashed <crashed001@gmail.com>
2025-10-27 10:14:49 +0100

    Created editable-text component

---
+++ src/app/components/shared/editable-text/editable-text.ts
@@ -0,0 +1,153 @@
+import {Component, computed, effect, input, OnInit, output, Signal, signal, WritableSignal} from '@angular/core';
+import {TranslatePipe, TranslateService} from '@ngx-translate/core';
+import {FormControl, FormGroup, ReactiveFormsModule, ValidatorFn} from '@angular/forms';
+
+@Component({
+  selector: 'app-editable-text',
+  imports: [
+    TranslatePipe,
+    ReactiveFormsModule
+  ],
+  templateUrl: './editable-text.html',
+  styleUrl: './editable-text.css'
+})
+export class EditableText implements OnInit {
+  // Inputs
+  text = input<string>("");
+  minLength = input(20);
+  validators = input<ValidatorFn | ValidatorFn[] | undefined>([]);
+  verticalLayout = input<boolean>(false);
+
+  // Outputs
+  onEdit = output<string>();
+
+  processedText = computed(() =>
+    this.text().padEnd(this.minLength(), ' ')
+  );
+
+  private _editing = signal(false);
+  editing:Signal<boolean> = this._editing;
+
+  private _unsaved = signal("");
+  unsavedEdit = computed(() => this._unsaved().trim());
+
+  writeUnsaved(text: string): void {
+    const textVal = text.trim();
+    this._unsaved.set(textVal);
+  }
+
+  handleEditCancelBtn() {
+    this._editing.set(!this.editing());
+    if(this.editing()) {
+      this._unsaved.set(
+        this.text().trim() === "----" ? "" : this.text().trim()
+      );
+      this.editForm.get("textField")?.setValue(this.unsavedEdit());
+      this.UpdateErrors();
+    }
+  }
+
+  //////////
+  // form //
+  //////////
+
+  editForm: FormGroup;
+  private _formErrors:WritableSignal<string[]> = signal([]);
+  formErrors: Signal<string[]> = this._formErrors;
+  hasErrors = computed(()=> this.formErrors().length != 0);
+
+  constructor(private translate: TranslateService) {
+    // Form
+    this.editForm = new FormGroup({
+      "textField" : new FormControl(this.processedText(),
+        this.validators())
+    });
+
+    effect(() => {
+      this.editForm.clearValidators();
+      const validators = this.validators();
+      if(validators) {
+        this.editForm.get("textField")?.addValidators(validators);
+        this.editForm.updateValueAndValidity();
+      }
+    });
+
+    // Subscribe to follow error translations
+    //    Change will happen when error list is updated
+
+    this.translate.onLangChange.subscribe(() => {
+      this.UpdateErrorTranslations();
+    });
+  }
+
+  onSubmit() {
+    if(!this.editForm.valid) {
+      return;
+    }
+    this._editing.set(false);
+    this.onEdit.emit(this.unsavedEdit());
+  }
+
+  /////////////////
+  // Form errors //
+  /////////////////
+
+  private _errorTranslations= new Map(
+    [
+      ["field_empty", "Field can't be empty"],
+      ["field_regex", "Value must follow valid format"],
+      ["field_min_len", "Value has too few characters"],
+      ["field_max_len", "Value has too many characters"],
+    ]
+  );
+
+  ngOnInit() {
+    this.editForm.get('textField')?.valueChanges.subscribe((value:string) => {
+      this.writeUnsaved(value);
+      this.UpdateErrors();
+    });
+  }
+
+  private UpdateErrors() {
+    const control = this.editForm.get('textField');
+    const errors: string[] = [];
+
+    if (control?.hasError('required')) {
+      const translation:string = this._errorTranslations.get("field_empty") ?? "";
+      if(translation != "") {
+        errors.push(translation);
+      }
+    }
+
+    if (control?.hasError('minlength')) {
+      const translation:string = this._errorTranslations.get("field_min_len") ?? "";
+      if(translation != "") {
+        errors.push(translation);
+      }
+    }
+
+    if (control?.hasError('maxlength')) {
+      const translation:string = this._errorTranslations.get("field_max_len") ?? "";
+      if(translation != "") {
+        errors.push(translation);
+      }
+    }
+
+    if (control?.hasError('pattern') || control?.hasError('email')) {
+      const translation:string = this._errorTranslations.get("field_regex") ?? "";
+      if(translation != "") {
+        errors.push(translation);
+      }
+    }
+
+    this._formErrors.set(errors);
+  }
+
+  UpdateErrorTranslations() {
+    for(const translationKey of this._errorTranslations.keys()) {
+      this.translate.get(`validator_errors.${translationKey}`).subscribe((translatedText: string) => {
+        this._errorTranslations.set(translationKey, translatedText);
+      });
+    }
+  }
+}
