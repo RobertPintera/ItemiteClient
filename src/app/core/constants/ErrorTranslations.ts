c22a6b7a78695d17a03a1975df4a6ae1d644bd23
HAScrashed <crashed001@gmail.com>
2025-11-04 17:23:26 +0100

    Added missing ErrorTranslations file
    This file is for convinience. Its used by validation utility functions

---
+++ src/app/core/constants/ErrorTranslations.ts
@@ -0,0 +1,24 @@
+export const errorTranslations = new Map(
+  [
+    ["pass_min_len", "Password must be at least 7 characters long"],
+    ["pass_max_len", "Password cannot be more than 50 characters"],
+    ["pass_uppercase", "Password must contain at least one uppercase letter"],
+    ["pass_lowercase", "Password must contain at least one lowercase letter"],
+    ["pass_digit", "Password must contain at least one digit"],
+    ["pass_special", "Password must contain at least one special character"],
+    ["pass_match", "Passwords must match"],
+    ["phone_min_len", "Phone number must be at least 3 characters long"],
+    ["phone_max_len", "Phone number cannot be more than 20 characters"],
+    ["name_min_len", "Username must be at least 3 characters long"],
+    ["name_max_len", "Username cannot be more than 20 characters"],
+    ["name_pattern","Username can have only letters, digits and characters \"._@+-\""],
+    ["mail_min_len", "Mail must be at least 3 characters long"],
+    ["mail_max_len", "Mail cannot be more than 100 characters"],
+    ["field_empty", "Field can't be empty"],
+    ["field_regex","Value must follow valid format"],
+    ["field_min_len", "Value has too few characters"],
+    ["field_max_len", "Value has too many characters"],
+  ]
+);
+
+
