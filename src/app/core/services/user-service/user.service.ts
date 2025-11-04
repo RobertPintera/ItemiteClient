753e5831e169f3d84a866e0f18cafccf7845f9d3
HAScrashed <crashed001@gmail.com>
2025-11-04 17:33:01 +0100

    Started user service

--- src/app/core/services/user-service/user.service.ts
+++ src/app/core/services/user-service/user.service.ts
@@ -1,8 +1,43 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Injectable, Signal, signal} from '@angular/core';
+import {UserBasicInfo} from '../../models/UserBasicInfo';
+import {lastValueFrom, map, Observable} from 'rxjs';
+import {environment} from '../../../../.env';
+import {ErrorHandlerService} from '../error-handler-service/error-handler-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  
+
+  private _userBasicInfo = signal<UserBasicInfo>(
+    {
+      id : -1,
+      username : "",
+      email: ""
+    }
+  );
+  userBasicInfo: Signal<UserBasicInfo> = this._userBasicInfo;
+
+  isUserLoggedIn(): boolean {
+    return this._userBasicInfo().id !== -1;
+  }
+
+  constructor(private http: HttpClient, private errorHandlerService: ErrorHandlerService) {
+  }
+
+  // Returns true if successfully logged in, otherwise false
+  async Login(email: string, password: string) : Promise<boolean> {
+    const payload = {email: email, password: password};
+    try {
+      const userInfo = await lastValueFrom(
+        this.http.post<UserBasicInfo>(`${environment.itemiteApiUrl}/auth/login`, payload, {timeout: 3000})
+      );
+      this._userBasicInfo.set(userInfo);
+      return true;
+    } catch (error: any) {
+      this.errorHandlerService.SendErrorMessage(error);
+      return false;
+    }
+  }
 }
