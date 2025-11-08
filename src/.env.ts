b60bf252c50bbafc1a37a38746d11776ddd93a9c
HAScrashed <crashed001@gmail.com>
2025-11-08 10:49:25 +0100

    env -> environments

--- src/.env.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  geoapifyUrl: 'https://api.geoapify.com/v1/geocode',
-  geoapifyApiKey: 'Your api key',
-  itemiteApiUrl: 'http://localhost:5066/api',
-};
