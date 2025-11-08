2dbaa705c2730939f23ab509052606cd745229a5
HAScrashed <crashed001@gmail.com>
2025-11-08 19:42:35 +0100

    Added route for confirm email

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,33 +5,37 @@ import {LoginRegister} from './components/features/login-register/login-register
 import {ProductsList} from './components/features/products-list/products-list';
 import {ProductDetails} from './components/features/product-details/product-details';
 import {ProfilePage} from './components/features/profile-page/profile-page';
+import {ConfirmEmail} from './components/features/login-register/confirm-email/confirm-email';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayout,
     children: [
-      {
-        path: '',
-        component: Home,
-      },
-      {
-        path: 'products/:category',
-        component: ProductsList
-      },
-      {
-        path: 'products/:category/:slug',
-        component: ProductDetails
-      },
-      {
-        path: 'login',
-        component: LoginRegister
-      },
-      {
-        path: 'dev',
-        component: ProfilePage
-      }
-
-    ]
-  }
+        {
+          path: '',
+          component: Home,
+        },
+        {
+          path: 'products/:category',
+          component: ProductsList
+        },
+        {
+          path: 'products/:category/:slug',
+          component: ProductDetails
+        },
+        {
+          path: 'login',
+          component: LoginRegister
+        },
+        {
+          path: 'dev',
+          component: ProfilePage
+        }
+      ]
+    },
+    {
+      path: 'confirm-email',
+      component: ConfirmEmail
+    }
 ];
