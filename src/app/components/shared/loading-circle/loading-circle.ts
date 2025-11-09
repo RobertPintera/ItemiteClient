49399a76df3546750c22ceceef9ec45d955616f7
HAScrashed <crashed001@gmail.com>
2025-11-09 17:49:05 +0100

    Created loading circle component

---
+++ src/app/components/shared/loading-circle/loading-circle.ts
@@ -0,0 +1,10 @@
+import {Component, input} from '@angular/core';
+
+@Component({
+  selector: 'app-loading-circle',
+  imports: [],
+  templateUrl: './loading-circle.html',
+  styleUrl: './loading-circle.css'
+})
+export class LoadingCircle {
+}
