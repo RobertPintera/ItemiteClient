4cb6be8a7605c4150827545de5e2ad76015b2fef
HAScrashed <crashed001@gmail.com>
2025-11-16 09:33:35 +0100

    Updated user model according to model in API

---
+++ src/app/core/models/User.ts
@@ -0,0 +1,9 @@
+export interface User {
+  id: number,
+  userName: string,
+  email: string,
+  location: string | undefined,
+  phoneNumber: string | undefined,
+  photoUrl: string | undefined,
+  backgroundUrl: string | undefined
+}
