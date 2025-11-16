86d7c05fe4fef3f77cc9a9c87da921bc25125c73
HAScrashed <crashed001@gmail.com>
2025-11-16 09:32:15 +0100

    Fixed ErrorHandler

--- src/app/core/Utility/ErrorHandler.ts
+++ src/app/core/Utility/ErrorHandler.ts
@@ -3,10 +3,10 @@ import { HttpErrorResponse } from "@angular/common/http";
 // Returns array of [error description, error]
 export function ErrorHandler(error: HttpErrorResponse): Array<string> {
   console.log(error);
-  if (error.error.message) {
+  if (error.error && error.error.message) {
     // Error thrown by client
     return [error.error.message, error.status.toString()];
-  } else {
+  } else if(error.error) {
     // Error thrown by API
     let message:string = error.error.Message;
     if(error.error.Errors) {
@@ -19,5 +19,7 @@ export function ErrorHandler(error: HttpErrorResponse): Array<string> {
       }
     }
     return [message, error.status.toString()];
+  } else {
+    return [error.statusText, error.status.toString()];
   }
 }
