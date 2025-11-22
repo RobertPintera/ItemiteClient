32dabb4c5ed4fbdf9ed86f74985a46a5e154ea44
RobertPintera <robert.it.990@gmail.com>
2025-11-22 12:35:58 +0100

    Update localization logic for loading main categories

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,5 @@
-import {Component, inject, OnInit, signal} from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {CategoryService} from './core/services/category-service/category.service';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +9,6 @@ import {CategoryService} from './core/services/category-service/category.service
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-  protected readonly title = signal('ItemiteClient');
-  private categoryService = inject(CategoryService);
+export class App{
 
-  ngOnInit() {
-    this.categoryService.loadMainCategories().subscribe({
-      // next: categories => console.log('Main categories loaded'),
-      error: err => console.error(err)
-    });
-  }
 }
--- src/app/components/layouts/main-layout/main-layout.ts
+++ src/app/components/layouts/main-layout/main-layout.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {Header} from '../../shared/header/header';
 import {Navbar} from '../../shared/navbar/navbar';
 import {RouterOutlet} from '@angular/router';
 import {ErrorNotification} from '../../shared/error-notification/error-notification';
 import {Footer} from '../../shared/footer/footer';
+import {CategoryService} from '../../../core/services/category-service/category.service';
 
 @Component({
   selector: 'app-main-layout',
@@ -17,6 +18,13 @@ import {Footer} from '../../shared/footer/footer';
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css'
 })
-export class MainLayout {
+export class MainLayout implements OnInit {
+  private categoryService = inject(CategoryService);
 
+  ngOnInit() {
+    this.categoryService.loadMainCategories().subscribe({
+      // next: categories => console.log('Main categories loaded'),
+      error: err => console.error(err)
+    });
+  }
 }
