fd10f35d928b98b2b857104f7d3d4097127e3684
RobertPintera <robert.it.990@gmail.com>
2025-11-22 20:10:07 +0100

    Add 404 page

---
+++ src/app/components/features/not-found/not-found.ts
@@ -0,0 +1,21 @@
+import {Component, HostBinding, inject} from '@angular/core';
+import {Button} from '../../shared/button/button';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  imports: [
+    Button
+  ],
+  templateUrl: './not-found.html',
+  styleUrl: './not-found.css'
+})
+export class NotFound {
+  @HostBinding('class') hostClass = 'block';
+
+  private router = inject(Router);
+
+  goHome() {
+    this.router.navigate(['/']);
+  }
+}
