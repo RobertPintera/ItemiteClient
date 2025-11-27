1c1f95260895e10f627fefa25a7352b5595f4c86
HAScrashed <crashed001@gmail.com>
2025-11-27 10:20:25 +0100

    Added api queries for chat
    not tested!

--- src/app/core/services/message-service/message.service.ts
+++ src/app/core/services/message-service/message.service.ts
@@ -1,10 +1,12 @@
 import {inject, Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {lastValueFrom, Observable} from 'rxjs';
-import {Environment} from '@angular/cli/lib/config/workspace-schema';
 import {environment} from '../../../../environments/environment.development';
 import {ErrorHandlerService} from '../error-handler-service/error-handler-service';
 import {SendMessageResultDTO} from '../../models/chat/SendMessageResultDTO';
+import {ChatListResponseDTO} from '../../models/chat/ChatListResponseDTO';
+import {ChatResponseDTO} from '../../models/chat/ChatResponseDTO';
+import {MessageResponse} from '../../models/chat/MessageResponse';
 
 @Injectable(
   {
@@ -16,6 +18,42 @@ export class MessageService {
   private http = inject(HttpClient);
   private errorHandlerService = inject(ErrorHandlerService);
 
+  // region general chat queries
+  GetAllChats(
+    pageNumber: number,
+    pageSize: number = 10,
+  ): Observable<ChatListResponseDTO> {
+    const params = new HttpParams()
+      .set('pageNumber', pageNumber)
+      .set('pageSize', pageSize);
+    return this.http.get<ChatListResponseDTO>(`${environment.itemiteApiUrl}/message/chats`, {params:params, timeout: 15000, withCredentials: true});
+  }
+
+  GetChatsByListing(
+    listingId: number,
+    pageNumber: number,
+    pageSize: number = 10,
+  ): Observable<ChatListResponseDTO> {
+    const params = new HttpParams()
+      .set('pageNumber', pageNumber)
+      .set('pageSize', pageSize);
+    return this.http.get<ChatListResponseDTO>(`${environment.itemiteApiUrl}/message/${listingId}/chats`, {params:params, timeout: 15000, withCredentials: true});
+  }
+
+  GetChat(
+    listingId: number,
+    otherUserId: number,
+    pageNumber: number,
+    pageSize: number = 20,
+  ): Observable<ChatResponseDTO> {
+    const params = new HttpParams()
+      .set('pageNumber', pageNumber)
+      .set('pageSize', pageSize);
+    return this.http.get<ChatResponseDTO>(`${environment.itemiteApiUrl}/message/${listingId}/chats/${otherUserId}`, {params:params, timeout: 15000, withCredentials: true});
+  }
+  // endregion
+
+  // region message operations
   SendMessage (
     recipientId:number,
     listingId: number,
@@ -33,6 +71,28 @@ export class MessageService {
         formData.append('photos', file, file.name);
       });
     }
-    return this.http.post<SendMessageResultDTO>(`${environment.itemiteApiUrl}/message`, formData);
+    return this.http.post<SendMessageResultDTO>(`${environment.itemiteApiUrl}/message`, formData, {withCredentials: true});
   }
+
+  EditMessage(
+    messageId: number,
+    newContent : string,
+  ): Observable<MessageResponse> {
+    return this.http.put<MessageResponse>(`${environment.itemiteApiUrl}/message/${messageId}`, newContent, {timeout: 15000, withCredentials: true});
+  }
+
+  async DeleteMessage(
+    messageId: number
+  ) : Promise<boolean> {
+    try {
+      await lastValueFrom(
+        this.http.delete(`${environment.itemiteApiUrl}/message/${messageId}`, {timeout: 15000, withCredentials: true})
+      );
+      return true;
+    } catch (error: any) {
+      this.errorHandlerService.SendErrorMessage(error);
+      return false;
+    }
+  }
+  // endregion
 }
