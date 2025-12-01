d94d3ebae26dde5e4da5253b1d6a59c26fb0d516
HAScrashed <crashed001@gmail.com>
2025-12-01 08:54:11 +0100

    Updated test component

--- src/app/components/features/test/test.ts
+++ src/app/components/features/test/test.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, computed, inject, OnInit, signal} from '@angular/core';
 import {Chat} from '../chat/chat';
 import {ChatList} from '../chat/chat-list/chat-list';
+import {MessageService} from '../../../core/services/message-service/message.service';
+import {ChatListResponseDTO} from '../../../core/models/chat/ChatListResponseDTO';
+import {ChatInfoResponse} from '../../../core/models/chat/ChatInfoResponse';
 
 @Component({
   selector: 'app-test',
@@ -10,6 +13,16 @@ import {ChatList} from '../chat/chat-list/chat-list';
   templateUrl: './test.html',
   styleUrl: './test.css'
 })
-export class Test {
+export class Test implements  OnInit {
+  private messageService = inject(MessageService);
+  chatList = signal<ChatInfoResponse[]>([]);
+  hasChats = computed(() => {
+    return this.chatList().length != 0
+  });
 
+  ngOnInit() {
+    this.messageService.GetAllChats(1).subscribe(data => {
+      this.chatList.set(data.items);
+    })
+  }
 }
