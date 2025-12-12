662ddad5ef8bb5136f603b931bae4f02fca3c708
HAScrashed <crashed001@gmail.com>
2025-12-12 15:27:52 +0100

    Added rest of notifications for chat

--- src/app/core/services/notification-service/notification.service.ts
+++ src/app/core/services/notification-service/notification.service.ts
@@ -4,16 +4,25 @@ import { environment } from '../../../../environments/environment.development';
 import {UserService} from '../user-service/user.service';
 import {MessageResponse} from '../../models/chat/MessageResponse';
 import {Observable, Subject} from 'rxjs';
+import {ErrorHandlerService} from '../error-handler-service/error-handler-service';
+import {TranslateService} from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NotificationService {
-  private _hub: signalR.HubConnection;
+  // todo maybe keep retrying to connect back to hub
+
   private _userService = inject(UserService);
+  private _hub: signalR.HubConnection;
 
   private _onMessageReceived = new Subject<MessageResponse>();
+  private _onMessageDeleted = new Subject<number>();
+  private _onMessageUpdated = new Subject<MessageResponse>();
+
   get onMessageReceived() { return this._onMessageReceived.asObservable(); }
+  get onMessageDeleted() { return this._onMessageDeleted.asObservable(); }
+  get onMessageUpdated() { return this._onMessageUpdated.asObservable(); }
 
   constructor() {
     this._hub = new signalR.HubConnectionBuilder()
@@ -39,8 +48,8 @@ export class NotificationService {
     this._hub.start()
       .then(() => {
         console.log('SignalR Connection for notification service started');
-        this._hub.onreconnected(id => this.OnReconnect(id));
         this._hub.onclose(error => this.OnDisconnect(error));
+        this._hub.onreconnected(()=>this.OnReconnect());
       })
       .catch(err => console.error('Error establishing SignalR for notifications: ' + err));
 
@@ -49,27 +58,46 @@ export class NotificationService {
 
   private async Disconnect() {
     if(this._hub.connectionId === null) return;
-
     await this._hub.stop();
   }
 
   private OnDisconnect(error: Error | undefined) {
-
+    this.UnregisterMessageEvents();
   }
 
-  private OnReconnect(id: string | undefined) {
-
+  private OnReconnect() {
+    this.UnregisterMessageEvents();
+    console.log('SignalR Reconnected for notification service');
+    this.RegisterMessageEvents();
   }
+
 
   private RegisterMessageEvents() {
     this._hub.on("MessageReceived", (message: MessageResponse) => {
-      console.log("Received message " + message)
+      console.log("Received message: " + message)
       this._onMessageReceived.next(message);
     });
+
+    this._hub.on("MessageUpdated", (message: MessageResponse) => {
+      console.log("Edited message: " + message);
+      this._onMessageUpdated.next(message);
+    });
+
+    interface MessageDeletedResponse {
+      messageId: number,
+      messageDeletedString: string
+    }
+
+    this._hub.on("MessageDeleted", (message: MessageDeletedResponse) => {
+      console.log("Deleted message: " + message);
+      this._onMessageDeleted.next(message.messageId);
+    })
   }
 
   private UnregisterMessageEvents() {
     this._hub.off("MessageReceived");
+    this._hub.off("MessageUpdated");
+    this._hub.off("MessageDeleted");
   }
 
 }
