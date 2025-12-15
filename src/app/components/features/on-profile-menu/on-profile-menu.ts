25d1875362989d477e0d5c1c93c900e9ae14cfbe
HAScrashed <crashed001@gmail.com>
2025-12-15 14:00:45 +0100

    Created OnProfileMenu used in header for profile icon

--- src/app/components/features/on-profile-menu/on-profile-menu.ts
+++ src/app/components/features/on-profile-menu/on-profile-menu.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, computed, inject, input, output} from '@angular/core';
+import {TranslatePipe} from '@ngx-translate/core';
+import {TextIconMenuItem} from './text-icon-menu-item/text-icon-menu-item';
+import {UserService} from '../../../core/services/user-service/user.service';
+import {AuthService} from '../../../core/services/auth-service/auth.service';
+import {NotificationService} from '../../../core/services/notification-service/notification.service';
 
 @Component({
   selector: 'app-on-profile-menu',
-  imports: [],
+  imports: [
+    TranslatePipe,
+    TextIconMenuItem
+  ],
   templateUrl: './on-profile-menu.html',
   styleUrl: './on-profile-menu.css',
 })
 export class OnProfileMenu {
+  visible = input<boolean>(true);
+  onClose = output<void>();
 
+
+  private _authService = inject(AuthService);
+  private _notificationService = inject(NotificationService);
+
+  readonly notificationCount = computed(() =>
+    this._notificationService.notificationCount()
+  );
+
+  OnMenuItemClicked() {
+    this.onClose.emit();
+  }
+
+  OnNotificationsClicked() {
+    this.OnMenuItemClicked();
+  }
+
+  async OnLogoutClicked() {
+    this.OnMenuItemClicked();
+    await this._authService.Logout();
+  }
 }
