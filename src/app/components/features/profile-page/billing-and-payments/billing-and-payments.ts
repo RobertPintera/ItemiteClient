f542716ac8faa0124acd042c09b6b00a30ad54c9
RobertPintera <robert.it.990@gmail.com>
2025-12-20 17:08:16 +0100

    Add loading dialog

--- src/app/components/features/profile-page/billing-and-payments/billing-and-payments.ts
+++ src/app/components/features/profile-page/billing-and-payments/billing-and-payments.ts
@@ -1,28 +1,37 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {Button} from '../../../shared/button/button';
 import {TranslatePipe} from '@ngx-translate/core';
 import {PaymentService} from '../../../../core/services/payment-service/payment-service';
 import {RouterLink} from '@angular/router';
+import {LoadingDialog} from '../../../shared/loading-dialog/loading-dialog';
+import {finalize} from 'rxjs';
 
 @Component({
   selector: 'app-billing-and-payments',
   imports: [
     Button,
     TranslatePipe,
-    RouterLink
+    RouterLink,
+    LoadingDialog
   ],
   templateUrl: './billing-and-payments.html',
   styleUrl: './billing-and-payments.css',
 })
 export class BillingAndPayments {
-  private _paymentService = inject(PaymentService);
+  private paymentService = inject(PaymentService);
 
+  readonly loading = signal<boolean>(false);
 
   goToStripeAccount() {
-    this._paymentService.connectStripeStart().subscribe({
-      next: (response) => {
-        window.location.href = response.url;
-      },
-    });
+    this.loading.set(true);
+    this.paymentService.connectStripeStart()
+      .pipe(
+        finalize(() => this.loading.set(false))
+      )
+      .subscribe({
+        next: response => {
+          window.location.href = response.url;
+        }
+      });
   }
 }
