4f3a4cdabc47ea0b888c9ee308b30202fd839b9c
HAScrashed <crashed001@gmail.com>
2025-12-21 14:20:28 +0100

    Updated NotifcationService to handle global notifications

--- src/app/core/services/notification-service/notification.service.ts
+++ src/app/core/services/notification-service/notification.service.ts
@@ -7,8 +7,10 @@ import {lastValueFrom, Observable, Subject} from 'rxjs';
 import {ErrorHandlerService} from '../error-handler-service/error-handler-service';
 import {TranslateService} from '@ngx-translate/core';
 import {isPlatformBrowser, isPlatformServer} from '@angular/common';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {User} from '../../models/User';
+import {Notification} from '../../models/notification/Notification';
+import {NotificationResponseDTO} from '../../models/notification/NotificationResponseDTO';
 
 @Injectable({
   providedIn: 'root',
@@ -17,7 +19,9 @@ export class NotificationService {
   private _userService = inject(AuthService);
   private _hub: signalR.HubConnection;
   private _http = inject(HttpClient);
+  private _errorService = inject(ErrorHandlerService);
 
+  private _onNotificationReceived = new Subject<Notification>();
   private _onMessageReceived = new Subject<MessageResponse>();
   private _onMessageDeleted = new Subject<number>();
   private _onMessageUpdated = new Subject<MessageResponse>();
@@ -25,9 +29,13 @@ export class NotificationService {
   get onMessageReceived() { return this._onMessageReceived.asObservable(); }
   get onMessageDeleted() { return this._onMessageDeleted.asObservable(); }
   get onMessageUpdated() { return this._onMessageUpdated.asObservable(); }
+  get onNotificationReceived() { return this._onNotificationReceived.asObservable(); }
 
-  private _notificationCount = signal(3);
+  private _notificationCount = signal(0);
   readonly notificationCount = this._notificationCount.asReadonly();
+
+  private _showNotificationList = signal(false);
+  readonly showNotificationList = this._showNotificationList.asReadonly();
 
   private _platformId = inject(PLATFORM_ID);
 
@@ -46,6 +54,7 @@ export class NotificationService {
     if(isPlatformServer(this._platformId)) return;
 
     this.RegisterMessageEvents();
+    this.RegisterNotificationEvent();
     this.RegisterConnectionEvents();
 
     effect(() => {
@@ -74,7 +83,6 @@ export class NotificationService {
 
   private async Disconnect() {
     if (this._hub.state === signalR.HubConnectionState.Disconnected) return;
-    this.UnregisterMessageEvents();
     await this._hub.stop();
   }
 
@@ -91,6 +99,20 @@ export class NotificationService {
     this._hub.onclose(error => {
       console.log('SignalR for notification service closed', error);
     });
+  }
+
+  private RegisterNotificationEvent() {
+    this._hub.on("Notification", (notification: any) => {
+      console.log(notification);
+      this._onNotificationReceived.next(notification);
+      this._notificationCount.set(this._notificationCount()+1);
+    });
+  }
+
+  private MarkNotificationsAsRead(count: number) {
+    this._notificationCount.set(
+      Math.max(this._notificationCount()-count, 0)
+    );
   }
 
   private RegisterMessageEvents() {
@@ -135,12 +157,63 @@ export class NotificationService {
     }
   }
 
+  async FetchNotifications(pageSize: number, pageNumber: number)
+    : Promise<NotificationResponseDTO | undefined> {
 
+    const params = new HttpParams()
+      .set('PageSize', pageSize)
+      .set('PageNumber', pageNumber);
 
-  private UnregisterMessageEvents() {
-    this._hub.off("MessageReceived");
-    this._hub.off("MessageUpdated");
-    this._hub.off("MessageDeleted");
+    try {
+      const notifications = await lastValueFrom(
+        this._http.get<NotificationResponseDTO>(`${environment.itemiteApiUrl}/notification`,
+          {withCredentials: true, params: params})
+      );
+      // mark notifications as read
+      this.MarkNotificationsAsRead(
+        notifications.items.filter((notification: Notification) =>
+          notification.readAt === null
+        ).length
+      );
+      return notifications;
+    } catch (error: any) {
+      this._errorService.SendErrorMessage(error);
+      return undefined;
+    }
   }
 
+  async DeleteNotification(notificationId:number): Promise<boolean> {
+    try {
+      await lastValueFrom(
+        this._http.delete(`${environment.itemiteApiUrl}/notification/${notificationId}`,
+          {withCredentials: true})
+      );
+      return true;
+    } catch (error: any) {
+      this._errorService.SendErrorMessage(error);
+      return false
+    }
+  }
+
+  async DeleteNotifications(): Promise<boolean> {
+    try {
+      await lastValueFrom(
+        this._http.delete(`${environment.itemiteApiUrl}/notification`,
+          {withCredentials: true})
+      );
+      this._notificationCount.set(0);
+      return true;
+    } catch (error: any) {
+      this._errorService.SendErrorMessage(error);
+      return false
+    }
+  }
+
+  SetNotificationListVisibility(visible: boolean) {
+    this._showNotificationList.set(visible);
+  }
+
+  SwitchNotificationVisibility() {
+    this._showNotificationList.set(!this.showNotificationList());
+  }
 }
