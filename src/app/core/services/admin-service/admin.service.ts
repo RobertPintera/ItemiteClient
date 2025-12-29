bea818148ebd86912132d3f66a103df78b8cb4e4
HAScrashed <crashed001@gmail.com>
2025-12-29 13:36:17 +0100

    Created admin service

---
+++ src/app/core/services/admin-service/admin.service.ts
@@ -0,0 +1,100 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {PaginatedUsersResponseDTO} from '../../models/user/PaginatedUsersResponseDTO';
+import {lastValueFrom, Observable} from 'rxjs';
+import {ChatListResponseDTO} from '../../models/chat/ChatListResponseDTO';
+import {environment} from '../../../../environments/environment.development';
+import {ErrorHandlerService} from '../error-handler-service/error-handler-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+  private _http = inject(HttpClient);
+  private _errorHandlerService = inject(ErrorHandlerService);
+
+  // region Users
+  GetUsers(pageNumber: number, searchQuery: string | undefined = undefined, pageSize: number = 10): Observable<PaginatedUsersResponseDTO> {
+    const params = new HttpParams()
+      .set('PageNumber', pageNumber)
+      .set('PageSize', pageSize);
+    if(searchQuery) {
+      params.set('searchQuery', searchQuery);
+    }
+    return this._http.get<PaginatedUsersResponseDTO>(`${environment.itemiteApiUrl}/adminpanel/users`, {params:params, timeout: 15000, withCredentials: true})
+  }
+
+  async LockUser(userId: number, lockoutEnd: string, lockoutMessage: string): Promise<boolean> {
+    const payload = {
+      userToLockoutId: userId,
+      lockoutEnd: lockoutEnd,
+      lockoutMessage: lockoutMessage
+    }
+    try {
+      await lastValueFrom(
+        this._http.post(`${environment.itemiteApiUrl}/adminpanel/lock-user`, payload, {timeout: 15000, withCredentials: true})
+      );
+      return true;
+    } catch (error: any) {
+      this._errorHandlerService.SendErrorMessage(error);
+      return false;
+    }
+  }
+
+  async UnlockUser(userId: number, unlockMessage: string): Promise<boolean> {
+    const payload = {
+      userId: userId,
+      unlockMessage: unlockMessage
+    }
+    try {
+      await lastValueFrom(
+        this._http.post(`${environment.itemiteApiUrl}/adminpanel/unlock-user`, payload, {timeout: 15000, withCredentials: true})
+      );
+      return true;
+    } catch (error: any) {
+      this._errorHandlerService.SendErrorMessage(error);
+      return false;
+    }
+  }
+  // endregion
+
+  // region Notifications
+  async SendGlobalNotification(emailSubject: string, title: string, message: string): Promise<boolean> {
+    const payload = {
+      emailSubject: emailSubject,
+      title: title,
+      message: message
+    }
+    try {
+      await lastValueFrom(
+        this._http.post(`${environment.itemiteApiUrl}/adminpanel/global-notification`, payload, {timeout: 15000, withCredentials: true})
+      );
+      return true;
+    } catch (error: any) {
+      this._errorHandlerService.SendErrorMessage(error);
+      return false;
+    }
+  }
+  async SendNotification(userId: number, emailSubject: string, title: string, message: string): Promise<boolean> {
+    const payload = {
+      emailSubject: emailSubject,
+      title: title,
+      message: message
+    }
+    try {
+      await lastValueFrom(
+        this._http.post(`${environment.itemiteApiUrl}/adminpanel/notification/${userId}`, payload, {
+          timeout: 15000,
+          withCredentials: true
+        })
+      );
+      return true;
+    } catch (error: any) {
+      this._errorHandlerService.SendErrorMessage(error);
+      return false;
+    }
+  }
+
+  // endregion
+
+}
