5f5a8aa5c177d8785a74c666d627c406bb909efc
HAScrashed <crashed001@gmail.com>
2026-01-02 17:34:40 +0100

    Translated admin panel

--- src/app/components/features/admin-panel/admin-panel.ts
+++ src/app/components/features/admin-panel/admin-panel.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {Button} from '../../shared/button/button';
 import {RouterLink} from '@angular/router';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-admin-panel',
   imports: [
     Button,
-    RouterLink
+    RouterLink,
+    TranslatePipe
   ],
   templateUrl: './admin-panel.html',
   styleUrl: './admin-panel.css',
